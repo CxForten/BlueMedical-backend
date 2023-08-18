899987554838e37d55a1a46937fa6fc8f3247915
Jandry-Alcivar <jandrypalacios2017@gmail.com>
2023-08-17 17:55:56 -0900

    Se_impleto_seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            UserSeeder::class,
+            GenderSeeder::class,
+            SpecialtySeeder::class,
+            SchedulesSeeder::class,
+            PersonSeed::class,
+            DoctorSeed::class,
+            //PatientSeed::class
+        ]);
+    
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AppointmentRequestCitaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\SpecialtiesController;
+use App\Http\Controllers\GenderController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PersonaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +21,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::apiResource('/appointment', AppointmentRequestCitaController::class)->only('index', 'store', 'show', 'update', 'destroy');
+    Route::apiResource('/doctor', DoctorController::class)->only('index', 'update', 'destroy');
+    Route::get('/doctor/{direction}/{specialty}/{schedules}', [DoctorController::class, 'show']);
+
+    Route::apiResource('/gender', GenderController::class)->only('index', 'show', 'update', 'destroy');
+    Route::apiResource('/specialties', SpecialtiesController::class)->only('index', 'show', 'update', 'destroy');
+    Route::apiResource('/schedules', ScheduleController::class)->only('index', 'show', 'update', 'destroy');
+    Route::apiResource('/persons', PersonaController::class)->only('index', 'store', 'show', 'update', 'destroy');
+    Route::apiResource('/patient', PatientController::class)->only('index', 'store', 'show', 'update', 'destroy');
 });
